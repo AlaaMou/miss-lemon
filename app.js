075507af9e4d0eeb20c67ea8420cfa11bfd1fbc1
Alaa <alaa.moussa2018@gmail.com>
2019-03-22 12:47:58 +0000

    edit favicon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const engine = require('ejs-mate');
 const indexRouter = require('./routes/index');
-
+const favicon = require('serve-favicon');
 
 const app = express();
 
@@ -16,6 +16,7 @@ app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(logger('dev'));
 app.use(express.json());
